289ace3107eafb41d1d762267088aac6e3a10653
Bagas <91589286+bgas1212@users.noreply.github.com>
2023-11-07 14:24:19 +0700

    Add files via upload

---
+++ LatihanJS/app.js
@@ -0,0 +1,11 @@
+// user menkonfirmasi jika mereka mules
+let mules = confirm ("lagi mules?");
+
+//jika true maka muncul tulisan "berak"
+if(mules == true){
+    document.write("berak")
+}
+//jika false maka muncul tulisan "gak berak"
+else{
+    document.write("gak berak")
+}
